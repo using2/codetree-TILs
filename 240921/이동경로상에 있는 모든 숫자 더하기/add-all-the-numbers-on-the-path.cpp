eda83cc65dcc1c9150e2494e9d508bfc26f95c36
강유진 <136972840+using2@users.noreply.github.com>
2024-09-21 20:22:38 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240921/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.cpp
+++ 240921/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.cpp
@@ -12,45 +12,42 @@ int main() {
 
     vector<vector<int>> arr(n, vector<int>(n, 0));	
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            int score;
-            cin >> score;
-            arr[i][j] = score;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
         }
     }
 
-    int x = (n - 1)/2;
+    int x = (n - 1) / 2;
     int y = x;
-    int direction[4][2] = {{0, -1}, {1, 0}, {-1, 0}, {-1, 0}};
+
+    int direction[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
     int directIdx = 0;
 
     result += arr[x][y];
 
-    int curDx = 0, curDy = 0;
-
-    for(int i = 0; i < args.length(); i++) {
-        switch(args[i]) {
+    for (int i = 0; i < args.length(); i++) {
+        switch (args[i]) {
             case 'R':
-                directIdx = (++directIdx) % 4;
+                directIdx = (directIdx + 1) % 4;
                 break;
-            case 'F':
-                curDx = direction[directIdx][0];
-                curDy = direction[directIdx][1];
-                if((x + curDx > -1) && (x + curDx < n) && (y + curDy > -1) && (y + curDy < n)) {
-                    x += curDx;
-                    y += curDy;
-                    result += arr[y][x];
-                    arr[y][x] = 0;
-                }
-                break;
-            case 'L': 
+            case 'L':
                 directIdx = (directIdx + 3) % 4;
+                break;
+            case 'F': 
+                int newX = x + direction[directIdx][0];
+                int newY = y + direction[directIdx][1];
+                
+                if (newX >= 0 && newX < n && newY >= 0 && newY < n) {
+                    x = newX;
+                    y = newY;
+                    result += arr[x][y];
+                }
                 break;
         }
     }
 
-    cout << result;
+    cout << result << endl;
 
     return 0;
 }
